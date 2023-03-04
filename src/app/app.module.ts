a8794a09c27eea1a70874b2c458ca7f63642711e
Terry0910 <105627355+Terry0910@users.noreply.github.com>
2023-03-05 00:18:32 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+const pageRoutes:Routes=[
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path:'projects', component: ProjectsComponent},
+  {path:'contact', component: ContactComponent}
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NavbarComponent,
+    ProjectsComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(pageRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
